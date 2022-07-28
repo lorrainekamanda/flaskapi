49479ba51f48f6135d3a23fcc8e71ab0841b64d6
Rain <user@localhost.localdomain>
2022-07-27 16:28:36 -0900

    add runtime

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,redirect,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import psycopg2
-from flask_heroku import Heroku
+
 import logging
 
 
@@ -12,7 +12,7 @@ app.config['SECRET_KEY'] = '01fcfdbeca8650acf466a53d4b45f24e'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://lorrainekamanda:leilanjeri@localhost/car'
 db = SQLAlchemy(app)
 migrate  = Migrate(app,db)
-heroku = Heroku(app)
+
 
 app.logger.addHandler(logging.streamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
