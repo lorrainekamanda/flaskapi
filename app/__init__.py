110900a19e6c2b133dcc2d5f12f0e81be661da95
Rain <user@localhost.localdomain>
2022-07-27 15:51:17 -0900

    switch

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,redirect,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import psycopg2
-
+from flask_heroku import Heroku
 
 
 app = Flask(__name__)
@@ -11,6 +11,6 @@ app.config['SECRET_KEY'] = '01fcfdbeca8650acf466a53d4b45f24e'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://lorrainekamanda:leilanjeri@localhost/car'
 db = SQLAlchemy(app)
 migrate  = Migrate(app,db)
-
+heroku = Heroku(app)
 
 from app import views
