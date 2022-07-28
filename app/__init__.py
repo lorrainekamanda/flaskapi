1adecb1da81a763b609f169995e668b27b7cd8be
Rain <user@localhost.localdomain>
2022-07-27 17:29:50 -0900

    postgresqL

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,11 @@ import logging
 
 app = Flask(__name__)
 
+prodURI = 'postgres://updljteaflhium:c0151e04752741e92119e1dc3c19132eaabe91ae3332085c0459d4dbae9b743e@ec2-44-208-88-195.compute-1.amazonaws.com:5432/d462304k9n45sm'
+prodURI = prodURI.replace("postgres://", "postgresql://")
 app.config['SECRET_KEY'] = '01fcfdbeca8650acf466a53d4b45f24e'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://updljteaflhium:c0151e04752741e92119e1dc3c19132eaabe91ae3332085c0459d4dbae9b743e@ec2-44-208-88-195.compute-1.amazonaws.com:5432/d462304k9n45sm'
+app.config['SQLALCHEMY_DATABASE_URI'] = prodURI
+
 db = SQLAlchemy(app)
 migrate  = Migrate(app,db)
 
